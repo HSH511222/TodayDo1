c72d82bdd11a25bb789dfb88510d74e910b3031a
Chaehyun Kim <chaehyuneee@gmail.com>
2025-12-14 15:28:05 +0900

    App.js 통계 추가

--- App.js
+++ App.js
@@ -3,9 +3,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Toast from 'react-native-toast-message';
-import { toastConfig } from './src/components/CustomToast';
-import { AuthProvider } from './src/core/context/authContext';
-import { UserProvider } from './src/core/context/userContext';
 
 import BackButton from './src/components/BackButton';
 import InitialScreen from './src/screens/InitialScreen';
@@ -21,9 +18,12 @@ import ForgotPWFin from './src/screens/ForgotPW_Fin';
 import ChangePW from './src/screens/ChangePW';
 import NotificationScreen from './src/screens/NotificationScreen';
 import BottomNavigator from './src/navigation/BottomNavigator';
-
-import { DataProvider } from './src/core/context/dataContext';
 import Statistics from './src/screens/Statistics';
+
+import { toastConfig } from './src/components/CustomToast';
+import { AuthProvider } from './src/core/context/authContext';
+import { UserProvider } from './src/core/context/userContext';
+import { DataProvider } from './src/core/context/dataContext';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -98,6 +98,16 @@ export default function App() {
                 <Stack.Screen
                   name="EmailVerificationPW"
                   component={EmailVerificationPW}
+                  options={({ navigation }) => ({
+                    title: '계정복구',
+                    headerLeft: () => <BackButton onPress={() => navigation.goBack()} />,
+                    headerShown: true,
+                    headerShadowVisible: false,
+                  })}
+                />
+                <Stack.Screen
+                  name="ForgotPWNewPW"
+                  component={ForgotPWNewPW}
                   options={({ navigation }) => ({
                     title: '계정복구',
                     headerLeft: () => <BackButton onPress={() => navigation.goBack()} />,
